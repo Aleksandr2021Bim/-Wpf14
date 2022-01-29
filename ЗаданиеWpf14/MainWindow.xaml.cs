b226de824786dbb386904198927acb32b0f022e2
Aleksandr Panarin <panarinalex12@gmail.com>
2022-01-29 15:03:59 +0300

    Добавьте файлы проекта.

---
+++ ЗаданиеWpf14/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ЗаданиеWpf14
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Product> products;
+        public MainWindow()
+        {
+            InitializeComponent();
+            products = new List<Product>();
+            products.Add(new Product()
+            {
+                Name = "Гречка",
+                Cost = 100,
+                Image = "Data/grecha.png",
+                Category = ProductsTypes.Food
+
+            });
+            products.Add(new Product()
+            {
+                Name = "Макароны",
+                Cost = 50,
+                Image = "Data/makaron.png",
+                Category = ProductsTypes.Food
+
+            });
+            products.Add(new Product()
+            {
+                Name = "Мыло",
+                Cost = 40,
+                Image = "Data/milo.png",
+                Category = ProductsTypes.Tech
+
+            });
+            lstBox.ItemsSource = products;
+        }
+    }
+}
---
+++ ЗаданиеWpf14/Tovar.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ЗаданиеWpf14
+{
+    public enum ProductsTypes
+    {
+        Food,
+        Tech
+    }
+
+    public class Product
+    {
+        public string Name { get; set; }
+        public double Cost { get; set; }
+        public string Image { get; set; }
+        public ProductsTypes Category { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+    }
+}
